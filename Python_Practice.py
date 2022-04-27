219ec620e298d10b99ac4ebd55ab31b768a6d8b5
AnaMMoreira <cinzasdanjo@hotmail.com>
2022-04-27 17:53:42 -0400

    adding PyPoll.py file.

---
+++ Python_Practice.py
@@ -0,0 +1,153 @@
+#print ("Hello World")
+counties = ["Arapahoe","Denver","Jefferson"]
+if counties[1] == 'Denver':
+    print(counties[1])
+
+    #What is the score?
+score = int(input("What is your test score? "))
+
+# Determine the grade.
+if score >= 90:
+    print('Your grade is an A.')
+else:
+    if score >= 80:
+        print('Your grade is a B.')
+    else:
+        if score >= 70:
+            print('Your grade is a C.')
+        else:
+            if score >= 60:
+                print('Your grade is a D.')
+            else:
+                print('Your grade is an F.')
+
+# What is the score?
+score = int(input("What is your test score? "))
+
+# Determine the grade.
+if score >= 90:
+    print('Your grade is an A.')
+elif score >= 80:
+    print('Your grade is a B.')
+elif score >= 70:
+    print('Your grade is a C.')
+elif score >= 60:
+    print('Your grade is a D.')
+else:
+    print('Your grade is an F.')
+
+# membership and logical operations
+
+    counties = ["Arapahoe","Denver","Jefferson"]
+if "El Paso" in counties:
+    print("El Paso is in the list of counties.")
+else:
+    print("El Paso is not the list of counties.")
+
+#check if either "Arapahoe" or "El Paso" is in the counties list
+if "Arapahoe" in counties or "El Paso" in counties:
+    print("Arapahoe or El Paso is in the list of counties.")
+else:
+    print("Arapahoe and El Paso are not in the list of counties.")
+
+#count controlled loops
+x = 0
+while x <= 5:
+    print(x)
+    x = x + 1
+
+for county in counties:
+    print(county)
+
+numbers = [0, 1, 2, 3, 4]
+for num in numbers:
+    print(num)
+
+for num in range(5):
+    print(num)
+
+for i in range(len(counties)):
+    print(counties[i])
+
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+
+for county in counties_dict:
+    print(county)
+
+#print Keys
+for county in counties_dict.keys():
+    print(county)
+
+#print values
+for voters in counties_dict.values():
+    print(voters)
+
+# print values using keys
+for county in counties_dict:
+    print(counties_dict[county])
+
+# print values using keys using .get
+for county in counties_dict:
+    print(counties_dict.get(county))
+
+#get key value pairs of dictionary
+#for key, value in dictionary_name.items():
+#   print(key, value)    
+for county, voters in counties_dict.items():
+    print(county, voters)
+#get dictionaryin list of dictionaries
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+                {"county":"Denver", "registered_voters": 463353},
+                {"county":"Jefferson", "registered_voters": 432438}]
+
+for county_dict in voting_data:
+    print(county_dict)
+
+#get values from a list of dictionarries
+for county_dict in voting_data:
+    for value in county_dict.values():
+        print(value)
+
+#print counties only
+for county_dict in voting_data:
+    print(county_dict['county'])
+
+#print formating of strings
+my_votes = int(input("How many votes did you get in the election? "))
+total_votes = int(input("What is the total votes in the election? "))
+percentage_votes = (my_votes / total_votes) * 100
+print("I received " + str(percentage_votes)+"% of the total votes.")
+
+#using f' comand
+my_votes = int(input("How many votes did you get in the election? "))
+total_votes = int(input("What is the total votes in the election? "))
+percentage_votes = (my_votes / total_votes) * 100
+print(f"I received " + str(percentage_votes)+"% of the total votes.")
+
+#using f strings with dictionaries
+counties_dict = {"Arapahoe": 369237, "Denver":413229, "Jefferson": 390222}
+for county, voters in counties_dict.items():
+    print(county + " county has " + str(voters) + " registered voters.")
+
+#or simpler...
+for county, voters in counties_dict.items():
+    print(f"{county} county has {voters} registered voters.")
+
+#multiple f strings
+candidate_votes = int(input("How many votes did the candidate get in the election? "))
+total_votes = int(input("What is the total number of votes in the election? "))
+message_to_candidate = (
+    f"You received {candidate_votes} number of votes. "
+    f"The total number of votes in the election was {total_votes}. "
+    f"You received {candidate_votes / total_votes * 100}% of the total votes.")
+
+print(message_to_candidate)
+
+#Format Floating-Point Decimals
+#f'{value:{width},.{precision}}'
+message_to_candidate = (
+    f"You received {candidate_votes:,} number of votes. "
+    f"The total number of votes in the election was {total_votes:,}. "
+    f"You received {candidate_votes / total_votes * 100:.2f}% of the total votes.")
+print(message_to_candidate)
+
